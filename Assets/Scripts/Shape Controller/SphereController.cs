835a50cfd8ba47ea93784eba3606fd2b66589d3b
Anthony Kazyaka <ask.anthony@hotmail.com>
2014-09-26 20:10:11 -0400

    Initial commit of project.

---
+++ Assets/Scripts/Shape Controller/SphereController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+    {
+        gameObject.rigidbody.AddTorque(0.0f, 0.0f, 1000.0f * Input.GetAxis("Horizontal") * Time.deltaTime, ForceMode.Acceleration);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.rigidbody.AddForce(0.0f, 100.0f, 0.0f, ForceMode.Force);
+        }
+	}
+}
